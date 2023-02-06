d64e50dbcd0e5933761e6a9320b71b43cbdde009
JorgePeju <pejuco@gmail.com>
2023-02-06 12:17:28 +0100

    12

---
+++ Lunes 6 Febrero/Ejercicio3.js
@@ -0,0 +1,16 @@
+  //Muestra los números del 0 al 9
+  for (let i = 0; i < 10 ; i = i + 1) {
+    console.log(i);
+}
+//Muestra los números del 1 al 10
+for (let i = 1; i <= 10 ; i = i + 1) {
+    console.log(i);
+}
+//Muestra los números del 1 al 10 con operador de post incremento
+for (let i = 1; i <= 10 ; i++) {
+    console.log(i);
+}
+//Muestra los números pares que hay entre 1 y 10
+for (let i = 2; i <= 10 ; i=i+2) {
+    console.log(i);
+}
