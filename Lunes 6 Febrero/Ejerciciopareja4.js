8b4da3ba1f763b69b6eec6542ad160cf53071357
JorgePeju <pejuco@gmail.com>
2023-02-07 06:41:10 +0100

    14

---
+++ Lunes 6 Febrero/Ejerciciopareja4.js
@@ -0,0 +1,13 @@
+//4. Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide (dispone de 3 intentos)
+var usuario1 = "Agapito";
+
+var pass1 = "Soldier";
+
+var usuario2 = prompt("Inserta tu Usuario: ");
+
+var pass2 = prompt("Inserta tu Contraseña: ");
+
+       
+
+
+
