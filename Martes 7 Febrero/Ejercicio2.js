4df08c7460e53db10b5f1cc1dd7eda601aa360b2
JorgePeju <pejuco@gmail.com>
2023-02-07 13:59:16 +0100

    2

---
+++ Martes 7 Febrero/Ejercicio2.js
@@ -0,0 +1,9 @@
+function entero() {
+    var n = parseInt(prompt("Inserta un número entero: "));
+    var cont = 0;
+
+    for (let i = 0; i < n.length; i++) {
+      
+    }
+    alert("Hay un total de "+ cont + "cifras")
+    }
