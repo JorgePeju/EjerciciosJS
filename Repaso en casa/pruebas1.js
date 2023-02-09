905ef275863d852d2482250132ef5e910c4673d6
JorgePeju <pejuco@gmail.com>
2023-02-09 08:15:17 +0100

    15

--- Repaso en casa/pruebas1.js
+++ Repaso en casa/pruebas1.js
@@ -1,16 +1,10 @@
-var n1 = parseFloat(prompt("Introduce el primer número: "));
-var n2 = parseFloat(prompt("Introduce el segundo número: "));
-var operacion = prompt("Introduzca '+' para sumbar o bien '-' para restar: ");
-var resultado = ""
+let num = prompt("Indica un número:");
+let cont = [];
+const asterisco = "*"
 
-if (operacion === '+') {
-    resultado = n1 + n2 
-} else 
-    if (operacion === '-') {
-    n1 - n2 
-    resultado = n1 - n2
-} else {
-    resultado = "Operación no válida"
+for (let i = 0; i < num; i++) {
+    cont[i] = asterisco;
+    console.log(cont);
 }
 
-alert("Tu resultado es el siguiente: " + resultado)
+alert(cont)
