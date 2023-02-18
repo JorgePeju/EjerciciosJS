fb67ba3f48818dd85ea9f4f9d2530b8b426315ad
JorgePeju <pejuco@gmail.com>
2023-02-18 17:44:25 +0100

    EJlógica18F

--- 17 Febrero Viernes/Ejerciciologica.js
+++ 17 Febrero Viernes/Ejerciciologica.js
@@ -1,58 +1,40 @@
 // Se solicita al usuario el nombre y 2 notas de x alumnos y se almacenan en un array
 // 1º Sacar la media de cada alumno
-// 2º Mostrar por pantalla la la media más alta y el nombre de los alumnos que tengan dicha media
+// 2º Mostrar por pantalla la media más alta y el nombre de los alumnos que tengan dicha media
 
-// Declaramos las variables primero
+// Declaramos las variables y constantes primero
 
 const arrayAlumnos = []; // un array de objetos que tendrá el nombre la nota 1 y la nota 2 y asignaremos la media.
 let mensaje;
 let mediaMayor;
-let alumnosMedia=[]; 
-let numAlumnos=parseInt(prompt("Introduzca el número de alumnos: "))  ;
+let alumnosMedia = [];
 
+function pedirDatosAlumnos() {
+  let numAlumnos = parseInt(prompt("Introduce el número de alumnos (al menos 1):"));
 
-// function validacionDatos() {
+  while (isNaN(numAlumnos) || numAlumnos < 1) {
+    numAlumnos = parseInt(prompt("Introduce un número válido de alumnos (al menos 1):"));
+  }
 
-// do { 
-    
-// } while (condition);
+  for (let i = 0; i < numAlumnos; i++) {
+    let nombre = prompt(`Introduce el nombre del alumno ${i + 1}:`);
+    let nota1 = parseFloat(prompt(`Introduce la primera nota de ${nombre} ${i + 1}:`));
+    let nota2 = parseFloat(prompt(`Introduce la segunda nota de ${nombre} ${i + 1}:`));
 
+    arrayAlumnos.push({ nombre, nota1, nota2 });
+  }
 
-// }
+  return alumnos;
+}
 
-//function solicitarDatos() {
+function validarDatosAlumnos(alumnos) {
+  for (let i = 0; i < alumnos.length; i++) {
+    let alumno = alumnos[i];
 
-  //  let numAlumnos=parseInt(prompt("Introduzca el número de alumnos: "))  ;
-
-    for (let i = 0; i < numAlumnos.length; i++) {
-        console.log(numAlumnos)
-    
-        let nombre=isNaN(prompt("Introduzca el nombre del alumno:"));
-        console.log(nombre)
-
-        do { nombre=isNaN(prompt("Introduzca el nombre del alumno:"))
-            
-        } while (nombre === isNaN);
-
-        let nota1=parseFloat(prompt("Introduzca la primera nota: "));
-        console.log(nota1)
-
-        do { nota1=parseFloat(prompt("Introduzca la primera nota: "));
-            
-        } while (nota1 != isNaN);
-
-        let nota2=parseFloat(prompt("Introduzca la segunda nota: "));
-
-        do { nota2=parseFloat(prompt("Introduzca la segunda nota: "));
-            
-        } while (nota2 != isNaN);
-        
-        almacenarDatos(nombre, nota1, nota2);
-        
+    if (!alumno.nombre || alumno.nota1 < 0 || alumno.nota1 > 10 || alumno.nota2 < 0 || alumno.nota2 > 10) {
+      return false;
     }
-//}
+  }
 
-// function almacenarDatos(nombre, nota1, nota2) {
-
-//     arrayAlumnos.push(nombre, nota1, nota2);
-// }
+  return true;
+}
