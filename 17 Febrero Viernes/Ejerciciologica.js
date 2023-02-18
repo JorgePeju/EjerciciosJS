33adaabf89c21b161f18f3a42e0aa5935c3130ec
JorgePeju <pejuco@gmail.com>
2023-02-18 19:47:59 +0100

    EJlógica18F

--- 17 Febrero Viernes/Ejerciciologica.js
+++ 17 Febrero Viernes/Ejerciciologica.js
@@ -10,26 +10,48 @@ let mediaMayor;
 let alumnosMedia = [];
 
 function pedirDatosAlumnos() {
+  // Pido los numeros de alumnos
   let numAlumnos = parseInt(prompt("Introduzca el número de alumnos:"));
-  let arrayAlumnos = [];
-
+  // Verifico que sea un número y válido
   while (isNaN(numAlumnos) || numAlumnos < 1) {
     numAlumnos = parseInt(prompt("Introduzca de nuevo un número de alumnos válidos:"));
   }
-
+  // Una vez tenemos el número de alumnos, pido los nombres y las notas.
   for (let i = 0; i < numAlumnos; i++) {
     let nombre = prompt(`Introduzca el nombre del alumno ${i + 1}:`);
     let nota1 = parseFloat(prompt(`Introduzca la primera nota de ${nombre} ${i + 1}:`));
+    // En caso de que no sea una nota correcta, bucle
     while (isNaN(nota1) || nota1 < 0 || nota1 > 10) {
       nota1 = parseFloat(prompt(`Introduzca una nota entre 0 y 10 para la primera nota de ${nombre} ${i + 1}:`));
     }
+    //Pido nota2
     let nota2 = parseFloat(prompt(`Introduzca la segunda nota de ${nombre} ${i + 1}:`));
+    // En caso de que no sea una nota correcta, bucle
     while (isNaN(nota2) || nota2 < 0 || nota2 > 10) {
       nota2 = parseFloat(prompt(`Introduzca una nota entre 0 y 10 para la segunda nota de ${nombre} ${i + 1}:`));
     }
+
+    console.log(arrayAlumnos)
     arrayAlumnos.push({ nombre, nota1, nota2 });
   }
 
   return arrayAlumnos;
+
+}
+
+
+function calcularMedias(arrayAlumnos) {
+
+  for (let i = 0; i < arrayAlumnos.length; i++) {
+    let posicion = arrayAlumnos[i];
+    console.log(posicion);
+    let mediaNota1 = posicion.nota1 / 2;
+    console.log(posicion.nota1);
+    let mediaNota2 = posicion.nota2 / 2;
+    let mediaTotal = (mediaNota1 + mediaNota2) / 2;
+    alumnosMedia.push({ nombre: posicion.nombre, mediaNota1, mediaNota2, mediaTotal });
+  }
+  console.log(alumnosMedia)
+  return alumnosMedia;
 }
 
