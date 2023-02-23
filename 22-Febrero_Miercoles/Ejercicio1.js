1896081a067a2db5c9c20add2e6c17f62b44c169
JorgePeju <pejuco@gmail.com>
2023-02-23 01:31:38 +0100

    ppt

--- 22-Febrero_Miercoles/Ejercicio1.js
+++ 22-Febrero_Miercoles/Ejercicio1.js
@@ -7,6 +7,8 @@ const piedraPapelTijera = () => {
     let papel = 3;
     const min = Math.ceil(1);
     const max = Math.floor(4);
+    let maquina = 0;
+    let jugador = 0;
      
 
     let resultado = Math.floor(Math.random() * (max - min) + min);
@@ -19,11 +21,11 @@ const piedraPapelTijera = () => {
     } else {
         if (resultado === piedra && usuario === tijera || resultado === tijera && usuario === papel || resultado === papel && usuario === piedra) {
             alert(`Has perdido`)
-            maquina++
+            maquina += 1
         } else {
             if (usuario === piedra && resultado === tijera || usuario === tijera && resultado === papel || usuario === papel && resultado === piedra) {
                 alert(`Has ganado`)
-                jugador++
+                jugador += 1
             
             }  return jugador, maquina
          
@@ -35,8 +37,50 @@ const piedraPapelTijera = () => {
 }
 
 function pintarDom(jugador,maquina) {
-    const puntosJugador = document.querySelector('#jugador');
-    const puntosMaquina = document.querySelector('#maquina');
+    document.getElementById("#jugador").innerHTML = `Jugador: ${jugador}`;
+    document.getElementById("#maquina").innerHTML = `Máquina: ${maquina}`;
 
 
 }
+
+
+
+
+// const piedraPapelTijera = (eleccionUsuario) => {
+//     let piedra = 1;
+//     let tijera = 2;
+//     let papel = 3;
+//     const min = Math.ceil(1);
+//     const max = Math.floor(4);
+    
+//     let resultado = Math.floor(Math.random() * (max - min) + min);
+  
+//     let usuario;
+//     if (eleccionUsuario === "piedra") {
+//       usuario = piedra;
+//     } else if (eleccionUsuario === "tijera") {
+//       usuario = tijera;
+//     } else if (eleccionUsuario === "papel") {
+//       usuario = papel;
+//     }
+  
+//     console.log(resultado);
+  
+//     if (resultado === piedra && usuario === piedra || resultado === tijera && usuario === tijera || resultado === papel && usuario === papel) {
+//       alert(`Empate`);
+//     } else {
+//       if (resultado === piedra && usuario === tijera || resultado === tijera && usuario === papel || resultado === papel && usuario === piedra) {
+//         alert(`Has perdido`);
+//         maquina += 1;
+//       } else {
+//         if (usuario === piedra && resultado === tijera || usuario === tijera && resultado === papel || usuario === papel && resultado === piedra) {
+//           alert(`Has ganado`);
+//           jugador += 1;
+//         }
+//       }
+//     }
+  
+//     document.getElementById("jugador").innerHTML = `Jugador: ${jugador}`;
+//     document.getElementById("maquina").innerHTML = `Máquina: ${maquina}`;
+
+//   };
